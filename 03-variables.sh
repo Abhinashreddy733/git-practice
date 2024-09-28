e120f7131c9eb01344a3826f8caf48228c371a71
Abhinashreddy733 <abhinashreddy693@gmail.com>
2024-09-28 13:08:28 +0530

    Variables

---
+++ 03-variables.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+#Example
+#Let's say x=0, y=1
+#formula derive
+#finally submit variables
+
+Person1: Ramesh
+Person2: Suresh
+
+echo "$Person1: Hi $Person2, how are you"
+echo "$Person2: Hello $Person1 I am fine. What about you!"
+echo "$Person1: I am doing good $Person2. Whats going on?"
+echo "$Person2: I started learning shell script $Person1"
