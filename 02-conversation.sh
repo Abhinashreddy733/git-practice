1f2fc990db49bf1399ff0092c2475e365259a742
Abhinashreddy733 <abhinashreddy693@gmail.com>
2024-09-28 12:51:43 +0530

    added connversation scrpit of ramesh & suresh

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
 
+echo "Ramesh: Hi Suresh, how are you"
+echo "Suresh: Hello ramesh I am fine. What about you!"
+echo "Ramesh: I am doing good suresh. Whats going on?"
+echo "Suresh: I started learning shell script ramesh"
