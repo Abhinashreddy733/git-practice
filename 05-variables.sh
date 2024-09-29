78453052ceeba714977a84dee7d9de0cd578139f
Abhinashreddy733 <abhinashreddy693@gmail.com>
2024-09-29 10:51:15 +0530

    Variables read command

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,7 +4,15 @@
 # It leads to security breach to avoid that we are usig read command.
 
 echo "Please enter the USERNAME:"
-
 read USERNAME     # Takes input into username variable
-
 echo "The username entered is :$USERNAME"
+
+# To hide the username while entering it to provide more security
+
+echo "Please enter the USERNAME:"
+read -s USERNAME     # Takes input into username variable
+echo "The username entered is :$USERNAME"
+
+echo "Please enter the PASSWORD:"
+read -s PASSWORD     # To hide the passowrd we use -s
+echo "The PASSWORD entered is :$PASSWORD"
