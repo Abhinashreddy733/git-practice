1c907cc73b8f96406e17e4383a4e0fb71ee8321f
Abhinashreddy733 <abhinashreddy693@gmail.com>
2024-09-29 11:57:40 +0530

    array

--- 06-array.sh
+++ 06-array.sh
@@ -2,7 +2,7 @@
 
 # Size starts from 0, size is 3
 
-FRUITS=("Apple" "KIWI" "ORANGE") Array
+FRUITS=("Apple" "KIWI" "ORANGE") # Array
 
 echo "First fruit is: ${FRUITS[0]}"
 echo "Second fruit is: ${FRUITS[1]}"
