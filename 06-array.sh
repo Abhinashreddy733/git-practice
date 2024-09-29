3e8e80f6450b263e6f6108073a54d7fde20e2f0d
Abhinashreddy733 <abhinashreddy693@gmail.com>
2024-09-29 11:52:34 +0530

    Variables read command

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Size starts from 0, size is 3
+
+FRUITS=("Apple" "KIWI" "ORANGE") Array
+
+echo "First fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+
+echo "All fruits are: ${FRUITS[@]}"
