4f327058873753dcb8829117a9c6443d36a866d2
Abhinashreddy733 <abhinashreddy693@gmail.com>
2024-09-30 12:37:53 +0530

    functions, short form comomands

---
+++ 09-special-vars.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "All variables passed to the script: $@"
+echo "Number of variables passed: $#"
+echo "Script name: $0"
+echo "Current working directory: $pwd"
+echo "Home directory of current user: $HOME"
+echo "PID(Process Id Instance) of the script executing now: $$"
+sleep 100 &  # It will goto sleep mode till 100sec. Till that time it will run in background
+echo "PID of last background activity or command: $!"
