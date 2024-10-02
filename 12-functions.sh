231c001518800933e31b3b3dce49425665523826
Abhinashreddy733 <abhinashreddy693@gmail.com>
2024-10-02 10:21:32 +0530

    Functions

---
+++ 12-functions.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+USERID=$(id -u)
+
+CHECK_ROOT()      #If we call the check root then it will get executed
+{
+    if ( $USERID -ne 0 )
+       then 
+       echo "Please run the script with root preveleges"
+       exit 1
+       fi
+}
+
+VALIDATE ()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is...FAILED"
+        exit 1
+    else
+        echo "$2 is...SUCCESS"
+fi
+
+CHECK_ROOT
+
+dnf list installed
+
+if ($? -ne 0)
+then 
+echo "GIT is not installed. Going to install it"
+      dnf install git -y
+      validate $? "Installing GIT"
+
+ else "Git is already installed"
+ fi
---
+++ 13-colours.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT()      #If we call the check root then it will get executed
+{
+    if ( $USERID -ne 0 )
+       then 
+       echo "Please run the script with root preveleges"
+       exit 1
+       fi
+}
+
+VALIDATE ()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"   # $N means ending that colour 
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N"
+fi
+
+CHECK_ROOT
+
+dnf list installed
+
+if ($? -ne 0)
+then 
+echo "GIT is not installed. Going to install it"
+      dnf install git -y
+      validate $? "Installing GIT"
+
+ else "Git is already installed"
+ fi
