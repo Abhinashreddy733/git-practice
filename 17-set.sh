cd0170a8f47776369d03b5f308d349309f5801d7
Abhinashreddy733 <abhinashreddy693@gmail.com>
2024-10-29 22:27:57 +0530

    shell

---
+++ 17-set.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -e
+# -e setting the automatic exit if we get error, set -ex for debug
+
+failur {
+echo "Failed at ($1:$2)"
+}
+
+trap 'failur "${LINENO}" "$BASH_COMMAND"' ERR # ERR is the error signal
+echo "Hello world success"
+echooo ""Hello world failure"
+echo "Hello world after failure"
